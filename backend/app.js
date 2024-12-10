6c71a545911a14e3ee18572ab48b316699e54f88
MicheleFumi <michele.fumi96@gmail.com>
2024-12-10 14:08:26 +0100

    add router for index

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const server = express()
-const movieController = require('./controllers/movieController.js')
 const host = process.env.HOST
 const port = process.env.PORT
-
+const movieRouter = require('./routes/movies.js')
 server.listen(port, () => {
     console.log(`Server is listening on port ${host}:${port}`);
 })
 
 
-server.get('/', movieController.index);
+server.use('/api/movies', movieRouter)
