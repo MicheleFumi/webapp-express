20b7d1d3fe6f9eb5ecc0a9457be436ebda8a28e6
MicheleFumi <michele.fumi96@gmail.com>
2024-12-10 14:23:43 +0100

    add show method

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const movieController = require('../controllers/movieController.js')
 
 router.get('/', movieController.index);
+router.get('/:id', movieController.show);
 
 module.exports = router;
